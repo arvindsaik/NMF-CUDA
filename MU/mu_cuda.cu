50f4a2d66144c167ef9462ae8f91c207d82bc5fd
arvindsaik <krishnanarvind100@gmail.com>
2017-11-13 23:45:59 +0530

    MU updated

--- MU/mu_cuda.cu
+++ MU/mu_cuda.cu
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
-
+#include <stdio.h>
 using namespace std;
 
 #define TILE_WIDTH 16
-#define TILE_DIM 32
-#define BLOCK_ROWS 8
+#define TILE_DIM 16
+#define BLOCK_ROWS 16
 
 // Compute C = A * B
 __global__ void matrixMultiply(float * A, float * B, float * C,
-  		       int numARows, int numAColumns,
-			       int numBRows, int numBColumns,
-			       int numCRows, int numCColumns) {
+             int numARows, int numAColumns,
+             int numBRows, int numBColumns,
+             int numCRows, int numCColumns) {
 
     __shared__ float ds_M[TILE_WIDTH][TILE_WIDTH];
     __shared__ float ds_N[TILE_WIDTH][TILE_WIDTH];
@@ -42,42 +42,46 @@ __global__ void matrixMultiply(float * A, float * B, float * C,
 }
 
 __global__ void
-transpose(float *odata, const float *idata, int rows, int cols)
+transpose(float *odata, float *idata, int rows, int cols)
 {
-  __shared__ float tile[TILE_DIM][TILE_DIM+1];
+  // __shared__ float tile[TILE_DIM][TILE_DIM];
 
   int x = blockIdx.x * TILE_DIM + threadIdx.x;
   int y = blockIdx.y * TILE_DIM + threadIdx.y;
   // int width = gridDim.x * TILE_DIM;
   int width = cols;
 
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-  {
-    if(y+j<rows && x<cols)
-      tile[threadIdx.y+j][threadIdx.x] = idata[(y+j)*width + x];
-    else
-      tile[threadIdx.y+j][threadIdx.x] = 0.0;
-  }
+  // for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+  // {
+  //   if(y+j<rows && x<cols)
+  //     tile[threadIdx.y+j][threadIdx.x] = idata[(y+j)*width + x];
+  //   else
+  //     tile[threadIdx.y+j][threadIdx.x] = 0.0;
+  // }
 
 
-  __syncthreads();
+  // __syncthreads();
 
-  x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
-  y = blockIdx.x * TILE_DIM + threadIdx.y;
+  // x = blockIdx.y * TILE_DIM + threadIdx.x;  // transpose block offset
+  // y = blockIdx.x * TILE_DIM + threadIdx.y;
 
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-  {
-    if(y+j<rows && x<cols)
-     odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
+  // for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+  // {
+  //   if(y+j<rows && x<cols)
+  //   { odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
+  //     printf("%f\n", odata[(y+j)*width + x]);
+  //   }
+  // }
+  if(x<cols && y<rows)
+  {  odata[x*rows + y] = idata[y*cols + x];
   }
 }
-
 __global__
 void elewisemulti(float *A, float *B, float *C, int rows, int cols)
 {
   int tx = threadIdx.x, ty = threadIdx.y,
-      bx = blockIdx.x, by = blockIdx.y,
-      row = by*blockDim.y+ty, col=blockDim.x*bx+tx;
+      bx = blockIdx.x, by = blockIdx.y;
+  int row = by*blockDim.y+ty, col=blockDim.x*bx+tx;
 
   if(row<rows && col<cols)
     C[row*cols + col] = A[row*cols + col]*B[row*cols + col];
@@ -87,8 +91,8 @@ __global__
 void elewisediv(float *A, float *B, float *C, int rows, int cols)
 {
   int tx = threadIdx.x, ty = threadIdx.y,
-      bx = blockIdx.x, by = blockIdx.y,
-      row = by*blockDim.y+ty, col=blockDim.x*bx+tx;
+      bx = blockIdx.x, by = blockIdx.y;
+  int row = by*blockDim.y+ty, col=blockDim.x*bx+tx;
 
   if(row<rows && col<cols)
     C[row*cols + col] = A[row*cols + col]/B[row*cols + col];
@@ -126,19 +130,19 @@ int main()
   memset(Ahost,0,m*n*sizeof(float));
 
   for(int i=0;i<100000;++i){
-	int temp;
-	dataset >> temp;
-	int x,y;
-	x = --temp;
-	dataset >> temp;
-	y = --temp;
-	dataset >> temp;
-	Ahost[x*n + y] = temp;
+  int temp;
+  dataset >> temp;
+  int x,y;
+  x = --temp;
+  dataset >> temp;
+  y = --temp;
+  dataset >> temp;
+  Ahost[x*n + y] = temp;
   }
 
   for(int i=0;i<m;++i){
     for(int j=0;j<k;++j){
-      Bhost[i*k + j] = rand()%2;
+      Bhost[i*k + j] = rand()%2+0.1;
       Bhost1[i*k + j] = Bhost[i*k + j];
     }
   }
@@ -146,7 +150,7 @@ int main()
   //randomising the output array C
   for(int i=0;i<k;++i){
     for(int j=0;j<n;++j){
-      Chost[i*n + j] = rand()%2;
+      Chost[i*n + j] = rand()%2+0.1;
       Chost1[i*n + j] = Chost[i*n + j];
     }
   }
@@ -171,10 +175,14 @@ int main()
   cudaMalloc((void **)&BCCtranspose, sizeof(float) * m*k);
   cudaMalloc((void **)&BACtranspose, sizeof(float) * m*k);
 
-  dim3 gridSize1((k-1)/TILE_DIM + 1,(n-1)/TILE_DIM + 1,1);
-  dim3 blockSize(TILE_DIM, BLOCK_ROWS, 1);
+  dim3 gridSize1((n-1)/16 + 1,(k-1)/16 + 1,1);
+  
+  dim3 blockSize1(16, 16, 1);
+  
+  dim3 blockSize2(TILE_WIDTH, TILE_WIDTH, 1);
 
-  dim3 gridSize2((m-1)/TILE_WIDTH + 1,(k-1)/TILE_WIDTH + 1,1);
+
+  dim3 gridSize2((k-1)/TILE_WIDTH + 1,(m-1)/TILE_WIDTH + 1,1);
 
   dim3 gridSize3((k-1)/TILE_WIDTH + 1,(k-1)/TILE_WIDTH + 1,1);
 
@@ -190,31 +198,53 @@ int main()
   cudaMalloc((void **)&BtransposeB, sizeof(float) * k*k);
   cudaMalloc((void **)&BtransposeBC, sizeof(float) * k*n);
 
-  dim3 gridSize4((m-1)/TILE_DIM + 1,(k-1)/TILE_DIM + 1,1);
+  dim3 gridSize4((k-1)/16 + 1,(m-1)/16 + 1,1);
 
-  dim3 gridSize5((k-1)/TILE_WIDTH + 1,(n-1)/TILE_WIDTH + 1,1);
+  dim3 gridSize5((n-1)/TILE_WIDTH + 1,(k-1)/TILE_WIDTH + 1,1);
 
   dim3 gridSize6((k-1)/TILE_WIDTH + 1,(k-1)/TILE_WIDTH + 1,1);
+
+  float *temp1 = new float[m*k];
 
   for(int i=0;i<epochs;i++)
   {
 
-    transpose<<<gridSize1, blockSize>>>(Ctranspose, C, k, n);
-    matrixMultiply<<<gridSize2, blockSize>>>(A, Ctranspose, ACtranspose, m, n, n, k, m, k);
-    elewisemulti<<<gridSize2, blockSize>>>(B, ACtranspose, BACtranspose, m, k);
+    transpose<<<gridSize1, blockSize1>>>(Ctranspose, C, k, n);
+    cudaDeviceSynchronize();
 
-    matrixMultiply<<<gridSize3, blockSize>>>(C, Ctranspose, CCtranspose, k, n, n, k, k, k);
-    matrixMultiply<<<gridSize2, blockSize>>>(B, CCtranspose, BCCtranspose, m, k, k, k, m, k);
-    elewisediv<<<gridSize2, blockSize>>>(BACtranspose, BCCtranspose, B, m, k);
 
-    transpose<<<gridSize4, blockSize>>>(Btranspose, B, m, k);
-    matrixMultiply<<<gridSize5, blockSize>>>(Btranspose, A, BtransposeA, k, m, m, n, k, n);
-    elewisemulti<<<gridSize5, blockSize>>>(C, BtransposeA, CBtransposeA, k, n);
 
-    matrixMultiply<<<gridSize6, blockSize>>>(Btranspose, B, BtransposeB, k, m, m, k, k, k);
-    matrixMultiply<<<gridSize2, blockSize>>>(BtransposeB, C, BtransposeBC, k, k, k, n, k, n);
-    elewisediv<<<gridSize2, blockSize>>>(CBtransposeA, BtransposeBC, C, k, n);
+    matrixMultiply<<<gridSize2, blockSize2>>>(A, Ctranspose, ACtranspose, m, n, n, k, m, k);
+    cudaDeviceSynchronize();
 
+    
+
+    elewisemulti<<<gridSize2, blockSize2>>>(B, ACtranspose, BACtranspose, m, k);
+    cudaDeviceSynchronize();
+
+    matrixMultiply<<<gridSize3, blockSize2>>>(C, Ctranspose, CCtranspose, k, n, n, k, k, k);
+    cudaDeviceSynchronize();
+
+    matrixMultiply<<<gridSize2, blockSize2>>>(B, CCtranspose, BCCtranspose, m, k, k, k, m, k);
+    cudaDeviceSynchronize();
+    elewisediv<<<gridSize2, blockSize2>>>(BACtranspose, BCCtranspose, B, m, k);
+    cudaDeviceSynchronize();
+
+    
+
+    transpose<<<gridSize4, blockSize1>>>(Btranspose, B, m, k);
+    cudaDeviceSynchronize();
+    matrixMultiply<<<gridSize5, blockSize2>>>(Btranspose, A, BtransposeA, k, m, m, n, k, n);
+    cudaDeviceSynchronize();
+    elewisemulti<<<gridSize5, blockSize2>>>(C, BtransposeA, CBtransposeA, k, n);
+    cudaDeviceSynchronize();
+
+    matrixMultiply<<<gridSize6, blockSize2>>>(Btranspose, B, BtransposeB, k, m, m, k, k, k);
+    cudaDeviceSynchronize();
+    matrixMultiply<<<gridSize2, blockSize2>>>(BtransposeB, C, BtransposeBC, k, k, k, n, k, n);
+    cudaDeviceSynchronize();
+    elewisediv<<<gridSize2, blockSize2>>>(CBtransposeA, BtransposeBC, C, k, n);
+    cudaDeviceSynchronize();
   }
 
   cudaMemcpy(Bhost, B, sizeof(float) * m*k, cudaMemcpyDeviceToHost);
@@ -245,6 +275,16 @@ int main()
   cudaFree(A);
   cudaFree(B);
   cudaFree(C);
+  cudaFree(ACtranspose);
+  cudaFree(Ctranspose);
+  cudaFree(CCtranspose);
+  cudaFree(BCCtranspose);
+  cudaFree(BACtranspose);
+  cudaFree(BtransposeA);
+  cudaFree(Btranspose);
+  cudaFree(CBtransposeA);
+  cudaFree(BtransposeB);
+  cudaFree(BtransposeBC);
 
   return 0;
 }
